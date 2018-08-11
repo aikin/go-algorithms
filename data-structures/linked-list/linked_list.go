56489e765657fafef0f314448d8fac08af3ffa91
aikin <1@aikin.me>
2018-08-11 16:33:55 +0800

    feat(LINKED LIST): init linked list

--- data-structures/linked-list/linked_list.go
+++ data-structures/linked-list/linked_list.go
@@ -1,1 +1,23 @@
 package list
+
+type List struct {
+	Length int
+	Head   *Node
+	Tail   *Node
+}
+
+type Node struct {
+	Value interface{}
+	Prev  *Node
+	Next  *Node
+}
+
+func NewNode(value interface{}) *Node {
+	return &Node{Value: value}
+}
+
+func NewList() *List {
+	l := new(List)
+	l.Length = 0
+	return l
+}
--- data-structures/linked-list/linked_list_test.go
+++ data-structures/linked-list/linked_list_test.go
@@ -1,1 +1,14 @@
 package list
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestLinkedList(t *testing.T) {
+	l := NewList()
+	fmt.Println(l)
+	if l.Length != 0 {
+		t.Fail()
+	}
+}
