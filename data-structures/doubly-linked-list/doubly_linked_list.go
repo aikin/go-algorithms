2c8cbfca4f6b4744d7aa7f5c28074f0ae3034770
aikin <1@aikin.me>
2018-10-03 16:26:28 +0800

    feat(DOUBLY LINKED LIST): should can apprend signle node into doubly linked list

--- data-structures/doubly-linked-list/doubly_linked_list.go
+++ data-structures/doubly-linked-list/doubly_linked_list.go
@@ -23,6 +23,17 @@ func NewDoublyLinkedList() *DoublyLinkedList {
 	return l
 }
 
-func (l *DoublyLinkedList)Len() int{
+func (l *DoublyLinkedList) Len() int {
 	return l.Length
+}
+
+func (l *DoublyLinkedList) Append(value interface{}) {
+	node := NewDoublyLinkedListNode(value)
+
+	if (l.Len() == 0) {
+		l.Head = node
+		l.Tail = l.Head
+	}
+
+	l.Length++
 }
--- data-structures/doubly-linked-list/doubly_linked_list_test.go
+++ data-structures/doubly-linked-list/doubly_linked_list_test.go
@@ -14,4 +14,27 @@ var _ = Describe("DoublyLinkedList", func() {
 			Ω(list.Len()).Should(BeZero())
 		})
 	})
+
+	Context("When append node to linked list", func() {
+		It("should append node to linked list", func() {
+			list := NewDoublyLinkedList()
+
+			Ω(list.Head).Should(BeNil())
+			Ω(list.Tail).Should(BeNil())
+
+			list.Append(1)
+
+			Ω(list.Len()).Should(Equal(1))
+
+			Ω(list.Head).Should(Not(BeNil()))
+			Ω(list.Head.Next).Should(BeNil())
+			Ω(list.Head.Previous).Should(BeNil())
+			Ω(list.Head.Value).Should(Equal(1))
+
+			Ω(list.Tail).Should(Not(BeNil()))
+			Ω(list.Tail.Next).Should(BeNil())
+			Ω(list.Tail.Previous).Should(BeNil())
+			Ω(list.Tail.Value).Should(Equal(1))
+		})
+	})
 })
