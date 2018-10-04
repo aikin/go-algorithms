0b4487ce4652bae7a3ef7d691c24120285621a84
aikin <1@aikin.me>
2018-10-04 17:05:46 +0800

    feat(QUEUE): init queue

---
+++ data-structures/queue/queue_test.go
@@ -0,0 +1,12 @@
+package queue_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/aikin/go-algorithms/data-structures/queue"
+)
+
+var _ = Describe("Queue", func() {
+
+})
