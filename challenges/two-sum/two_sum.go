e867ceea135fefb83f17215f83b38f62182a8450
Laijin Lu (ThoughtWorks) <ljlu@thoughtworks.com>
2020-07-20 22:23:53 +0800

    feat: 🎸 optimize algorithms

--- challenges/two-sum/two_sum.go
+++ challenges/two-sum/two_sum.go
@@ -21,42 +21,41 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 
 复杂度分析：
- - 时间复杂度：O(n^2)
- - 空间复杂度：O(1)O(1)
+ - 时间复杂度：O(n)：我们只遍历了包含有 nn 个元素的列表一次。在表中进行的每次查找只花费 O(1)O(1) 的时间。
+ - 空间复杂度：O(n)：所需的额外空间取决于哈希表中存储的元素数量，该表最多需要存储 nn 个元素。
 */
 func TwoSum(nums []int, target int) ([]int, error) {
-	var indices []int
+
+	numsMap := make(map[int]int)  // Num as key, Index as Value
+	indices := []int{}
 	exceptedIndicesSize := 2
 
+	for i, curNum := range nums {
+		_, existed := numsMap[curNum]
+		if (existed) {
+			return nil, errors.New("nums duplicates")
+		}
 
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-
-			hasDuplicated := nums[i] == nums[j]
-			if (hasDuplicated) {
-				return nil, errors.New("nums duplicates")
-			}
-
-			isOverflowSize := nums[i] + nums[j] <= nums[i] == (nums[j] > 0)
-			/*
+		isOverflowSize := target - curNum >= target == (curNum > 0)
+		/*
 			// isOverflowSize := (nums[i] + nums[j] <= nums[i] && nums[j] > 0) ||
 			// (nums[i] + nums[j] >= nums[i] && nums[j] < 0)
 			*/
-			if (isOverflowSize)  {
-				return nil, errors.New("should two sum in [min, max] rang")
-			}
-
-			isNotFull := len(indices) != exceptedIndicesSize
-			isTwoSumEqualTarget := nums[i] + nums[j] == target
-
-			if (isNotFull && isTwoSumEqualTarget) {
-				indices = append(indices, i, j)
-			}
-			if (!isNotFull && isTwoSumEqualTarget) {
-				return nil, errors.New("should exactly one solution")
-			}
+		if (isOverflowSize)  {
+			return nil, errors.New("should two sum in [min, max] rang")
 		}
-	}
 
+		isNotFull := len(indices) != exceptedIndicesSize
+		j, existed := numsMap[target - curNum]
+		if (isNotFull && existed) {
+			indices = append(indices, j, i)
+		}
+
+		if (!isNotFull && existed) {
+			return nil, errors.New("should exactly one solution")
+		}
+
+		numsMap[curNum] = i
+	}
 	return indices, nil
 }
