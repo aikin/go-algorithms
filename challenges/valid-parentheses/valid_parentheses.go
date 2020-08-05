cd4ff5ff9cd37b48482908d66ccdc1e8dd850d46
Laijin Lu (ThoughtWorks) <ljlu@thoughtworks.com>
2020-08-05 17:09:50 +0800

    refactor: 💡 make the if/else logic simple

--- challenges/valid-parentheses/valid_parentheses.go
+++ challenges/valid-parentheses/valid_parentheses.go
@@ -1,7 +1,14 @@
 package valid_parentheses
 
 /*
-* test
+Given s = [{}]
+
+Because is valid parentheses
+return true
+
+复杂度分析：
+ - 时间复杂度：O(n)
+ - 空间复杂度：O(n)，可能输入的 s 很长
 */
 func IsValid(s string) (bool, error) {
 	if (len(s) == 0) {
@@ -15,21 +22,25 @@ func IsValid(s string) (bool, error) {
 	}
 
 	chars := map[rune] rune {
-		')': '(',
-		'}': '{',
-		']': '[',
+		'(': ')',
+		'{': '}',
+		'[': ']',
 	}
 
 	stack := []rune{}
 
 	for _, char := range s {
-		if (char == '(' || char == '{' || char == '[') {
+		if _, ok := chars[char]; ok {
 			stack = append(stack, char)
-		} else if (len(stack) > 0 && stack[len(stack) - 1] == chars[char]) {
-			stack = stack[:len(stack) - 1]
-		} else {
-			return false, nil
+			continue
 		}
+		topChar := stack[len(stack) - 1]
+		matchedChar := chars[topChar]
+		if (len(stack) > 0 && matchedChar == char) {
+			stack = stack[:len(stack) - 1]
+			continue
+		}
+		return false, nil
 	}
 
 	return len(stack) == 0, nil
