54ea4fcb720cc58d92247aeffaa291b2c2260fca
Laijin Lu (ThoughtWorks) <ljlu@thoughtworks.com>
2020-08-05 20:29:29 +0800

    feat: 🎸 impl max subarray by DP way

--- challenges/maximum-subarray/maximum_subarray.go
+++ challenges/maximum-subarray/maximum_subarray.go
@@ -58,3 +58,35 @@ func MaxSubArrayByGreedyWay(nums []int) (int, error) {
 	}
 	return max, nil
 }
+
+/*
+Given nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+Because contiguous subarray [4,-1,2,1] has the largest sum = 6
+Then max = 6
+
+复杂度分析：
+ - 时间复杂度：O(n)
+ - 空间复杂度：O(n)
+*/
+func MaxSubArrayByDynamicWay(nums []int) (int, error) {
+	sums := make([]int, len(nums))
+	sums[0] = nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		if (sums[i - 1] < 0) {
+			sums[i] = nums[i]
+			continue
+		}
+		sums[i] = sums[i - 1] + nums[i]
+	}
+
+	max := minInt
+	for _, curSum := range sums {
+		if (curSum > max) {
+			max = curSum
+		}
+	}
+
+	return max, nil
+}
