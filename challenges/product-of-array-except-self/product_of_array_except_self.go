e314743593a107686765e3cdcff057216e3d029d
Laijin Lu (ThoughtWorks) <ljlu@thoughtworks.com>
2020-08-06 15:57:34 +0800

    feat: 🎸 add validate logic

--- challenges/product-of-array-except-self/product_of_array_except_self.go
+++ challenges/product-of-array-except-self/product_of_array_except_self.go
@@ -1,5 +1,15 @@
 package product_of_array_except_self
 
+import (
+	"errors"
+)
+
+const uintSize = 32 << (^uint(0) >> 32 & 1)
+const (
+	maxInt  = 1<<(uintSize-1) - 1
+	minInt  = -maxInt - 1
+	maxUint = 1<<uintSize - 1
+)
 
 /*
 Given nums = [1, 2, 3, 4]
@@ -12,21 +22,44 @@ Then result: [24, 12, 8, 6]
  - 空间复杂度：O(n)
 */
 func ProductExceptSelf(nums []int) ([]int, error) {
-	left, right, result := make([]int, len(nums)), make([]int, len(nums)), make([]int, len(nums))
+	left, right, products := make([]int, len(nums)), make([]int, len(nums)), make([]int, len(nums))
 
 	left[0] = 1
 	for i := 1; i < len(nums); i++ {
-		left[i] = left[i - 1] * nums[i - 1]
+		if isNumLesserThanOne(nums[i]) {
+			return nil, errors.New("the num > 1 is required")
+		}
+		if isProductOversize(left[i-1], nums[i-1]) {
+			return nil, errors.New("the num should not oversize int")
+		}
+		left[i] = left[i-1] * nums[i-1]
 	}
 
-	right[len(nums) - 1] = 1
+	right[len(nums)-1] = 1
 	for i := len(nums) - 2; i >= 0; i-- {
-		right[i] = right[i + 1] * nums[i + 1]
+		if isNumLesserThanOne(nums[i]) {
+			return nil, errors.New("the num > 1 is required")
+		}
+		if isProductOversize(right[i+1], nums[i+1]) {
+			return nil, errors.New("the num should not oversize int")
+		}
+		right[i] = right[i+1] * nums[i+1]
 	}
 
 	for i := 0; i < len(nums); i++ {
-		result[i] = left[i] * right[i]
+		if isProductOversize(left[i], right[i]) {
+			return nil, errors.New("the num should not oversize int")
+		}
+		products[i] = left[i] * right[i]
 	}
 
-	return result, nil
+	return products, nil
+}
+
+func isProductOversize(multiplier int, multiplicand int) bool {
+	return multiplier * multiplicand < multiplicand && (multiplicand > 1)
+}
+
+func isNumLesserThanOne(factor int) bool {
+	return factor < 1
 }
