278b55794de7e6338087c6c59f3d09541809d15d
Laijin Lu (ThoughtWorks) <ljlu@thoughtworks.com>
2020-08-06 15:57:34 +0800

    feat: 🎸 add validate logic

--- challenges/product-of-array-except-self/product_of_array_except_self_test.go
+++ challenges/product-of-array-except-self/product_of_array_except_self_test.go
@@ -32,5 +32,41 @@ var _ = Describe("ProductOfArrayExceptSelf", func() {
 				Ω(result).Should(Equal([]int {960, 480, 240, 384, 320}))
 			})
 		})
+
+		When("nums [2, 4, 0, 5, 6]", func() {
+			It("should return result: nil, err: the num > 1 required", func() {
+
+				nums := []int{2, 4, 0, 5, 6}
+
+				products, err := ProductExceptSelf(nums)
+
+				Ω(err).Should(MatchError("the num > 1 is required"))
+				Ω(products).Should(BeNil())
+			})
+		})
+
+		When("nums [2, -4, 0, 5, 6]", func() {
+			It("should return products: nil, err: the num > 1 required", func() {
+
+				nums := []int{2, -4, 0, 5, 6}
+
+				products, err := ProductExceptSelf(nums)
+
+				Ω(err).Should(MatchError("the num > 1 is required"))
+				Ω(products).Should(BeNil())
+			})
+		})
+
+		When("nums [99989989898899, 9989898898998, 99991232912199, 999123299, 3231232323]", func() {
+			It("should return result: nil, err: the num should not oversize int", func() {
+
+				nums := []int{99989989898899, 9989898898998, 99991232912199, 999123299, 3231232323}
+
+				products, err := ProductExceptSelf(nums)
+
+				Ω(err).Should(MatchError("the num should not oversize int"))
+				Ω(products).Should(BeNil())
+			})
+		})
 	})
 })
