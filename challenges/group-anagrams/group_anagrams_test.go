bb180a61ee901d2bc798a9454fd9545ed29a4287
Laijin Lu (ThoughtWorks) <ljlu@thoughtworks.com>
2020-08-22 15:32:57 +0800

    feat: 🎸 complex anaylic

--- challenges/group-anagrams/group_anagrams_test.go
+++ challenges/group-anagrams/group_anagrams_test.go
@@ -9,14 +9,25 @@ import (
 
 var _ = Describe("GroupAnagrams", func() {
 	Context("group anagrams general logic", func() {
-		When("numbres ['eat', 'tea', tan', 'ate', 'nat', 'bat']", func() {
-			It("should return groupedAnagrams: [['ate', 'eat', 'tea'], ['nat', 'tan'], ['bat']]", func() {
+		When("numbres ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']", func() {
+			It("should return groupedAnagrams: [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]", func() {
 
 				var anagrams = [] string{"eat", "tea", "tan", "ate", "nat", "bat"}
 
 				groupedAnagrams := GroupAnagrams(anagrams)
 
 				Ω(groupedAnagrams).Should(Equal([][]string{{"eat", "tea", "ate"}, {"tan", "nat"}, {"bat"}}))
+			})
+
+			When("numbres ['are', 'nat', 'tan', 'ear', 'code', 'era']", func() {
+				It("should return groupedAnagrams: [['ate', 'eat', 'tea'], ['nat', 'tan'], ['bat']]", func() {
+
+					var anagrams = [] string{"are", "nat", "tan", "ear", "code", "era"}
+
+					groupedAnagrams := GroupAnagrams(anagrams)
+
+					Ω(groupedAnagrams).Should(Equal([][]string{{"are", "ear", "era"}, {"nat", "tan"}, {"code"}}))
+				})
 			})
 		})
 	})
