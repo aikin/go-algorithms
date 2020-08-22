60b64d28539afdaae188fff35fc9c8676a06c754
Laijin Lu (ThoughtWorks) <ljlu@thoughtworks.com>
2020-08-22 15:32:57 +0800

    feat: 🎸 complex anaylic

--- challenges/group-anagrams/group_anagrams.go
+++ challenges/group-anagrams/group_anagrams.go
@@ -19,11 +19,13 @@ notes:
 - 不考虑答案输出的顺序。
 
 复杂度分析：
- - 时间复杂度：O(1)
- - 空间复杂度：O(1)
+ - 时间复杂度：O(NKlogK)
+	 其中 N 是 strs 的长度，而 K 是 strs 中字符串的最大长度。
+	 当我们遍历每个字符串时，外部循环具有的复杂度为 O(N)。
+	 然后，我们在 O(KlogK) 的时间内对每个字符串排序。
+ - 空间复杂度：O(NK)
 */
 func GroupAnagrams(strs []string) [][]string {
-
 	ans := make(map[string][]string)
 
 	for i := 0; i < len(strs); i++ {
