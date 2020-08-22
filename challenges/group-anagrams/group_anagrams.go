57bddac495797b01a47d48279aaa705474b8d3b4
Laijin Lu (ThoughtWorks) <ljlu@thoughtworks.com>
2020-08-22 15:54:16 +0800

    feat: 🎸 use counter way

--- challenges/group-anagrams/group_anagrams.go
+++ challenges/group-anagrams/group_anagrams.go
@@ -25,8 +25,57 @@ notes:
 	 然后，我们在 O(KlogK) 的时间内对每个字符串排序。
  - 空间复杂度：O(NK)
 */
-func GroupAnagrams(strs []string) [][]string {
+func GroupAnagramsBySort(strs []string) [][]string {
 	ans := make(map[string][]string)
+
+	for _, curAnagram := range strs {
+		key := sortForKeyOf(curAnagram)
+		value, ok := ans[key]
+		if (ok) {
+			ans[key] = append(value, curAnagram)
+			continue
+		}
+		ans[key] = []string {curAnagram}
+	}
+
+	return valuesFor(ans)
+}
+
+func sortForKeyOf(s string) string {
+	sChars := strings.Split(s, "")
+	sort.Strings(sChars)
+	return strings.Join(sChars, "")
+}
+
+func valuesFor(m map[string][]string) [][]string {
+	v := make([][]string, 0, len(m))
+	for  _, value := range m {
+	 v = append(v, value)
+	}
+	return v
+}
+
+/*
+Given anagrams = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+Because group anagrams [e, a, t] = [t, e, a]
+Then [
+	["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+notes:
+- 所有输入均为小写字母。
+- 不考虑答案输出的顺序。
+
+复杂度分析：
+ - 时间复杂度：O(NK)
+	其中 N 是 strs 的长度，而 K 是 strs 中字符串的最大长度。
+	计算每个字符串的字符串大小是线性的，我们统计每个字符串。
+ - 空间复杂度：O(NK)
+*/
+func GroupAnagrams(strs []string) [][]string {
+	ans := make(map[[26]int][]string)
 
 	for _, curAnagram := range strs {
 		key := keyOf(curAnagram)
@@ -41,13 +90,15 @@ func GroupAnagrams(strs []string) [][]string {
 	return values(ans)
 }
 
-func keyOf(s string) string {
-	sChars := strings.Split(s, "")
-	sort.Strings(sChars)
-	return strings.Join(sChars, "")
+func keyOf(s string) [26]int {
+	counter := [26]int{}
+	for _, cur := range s {
+		counter[cur - 'a']++
+	}
+	return counter
 }
 
-func values(m map[string][]string) [][]string {
+func values(m map[[26]int][]string) [][]string {
 	v := make([][]string, 0, len(m))
 	for  _, value := range m {
 	 v = append(v, value)
