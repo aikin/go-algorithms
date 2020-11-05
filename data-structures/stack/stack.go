606feafd58ef9fd23e1405cd2b0a76490a9f16b7
aikin <1@aikin.me>
2020-11-05 22:49:41 +0800

    feat: impl new stack function

--- data-structures/stack/stack.go
+++ data-structures/stack/stack.go
@@ -1,2 +1,23 @@
 package stack
 
+type Stack struct {
+
+	stack []interface{}
+
+	len int
+}
+
+func NewStack() *Stack {
+	stack := &Stack{}
+	stack.stack = make([]interface{}, 0)
+	stack.len = 0
+	return stack
+}
+
+func (stack *Stack) Len() int {
+	return stack.len
+}
+
+func (stack *Stack) IsEmpty() bool {
+	return stack.len == 0
+}
