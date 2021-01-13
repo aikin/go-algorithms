f57d25bd742029522d0abaf8fc75d0a3fe65efae
aikin <1@aikin.me>
2021-01-13 13:34:25 +0800

    feat: 🎸 impl by binary search way

--- challenges/find-minimum-in-rotated-sorted-array/README.md
+++ challenges/find-minimum-in-rotated-sorted-array/README.md
@@ -1,1 +1,14 @@
-https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
+# Find minimum in Rotated sorted array
+
+
+[leetcode](https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/)
+
+## Tasking
+* 二分法查找，前提条件，需要有一个有序的数组。
+* Roated sorted array，有一个特点，有序数组被分段，被分成了两个有序的数组。
+  * 所有变化点左侧元素 > 数组第一个元素
+  * 所有变化点右侧元素 < 数组第一个元素
+### impl
+* 校验
+* 圈复杂度
+* 边界 case
--- challenges/find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
+++ challenges/find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -7,9 +7,13 @@ THEN: 1
 
 复杂度分析：
  - 时间复杂度：O(n)
- - 空间复杂度：O(n)
+ - 空间复杂度：O(1)
 */
 func FindMinByForceWay(nums []int) int {
+	if (len(nums) == 1) {
+		return nums[0]
+	}
+
 	min := nums[0]
 	for i := 1; i < len(nums); i++ {
 		if (nums[i] < min) {
@@ -17,4 +21,38 @@ func FindMinByForceWay(nums []int) int {
 		}
 	}
 	return min
+}
+
+/*
+复杂度分析：
+ - 时间复杂度：O(logn)
+ - 空间复杂度：O(1)
+*/
+func FindMinByBinarySearch(nums []int) int {
+	if (len(nums) == 1) {
+		return nums[0]
+	}
+
+	left := 0
+	right := len(nums) - 1
+
+	if (nums[right] > nums[0]) {
+		return nums[0]
+	}
+
+	for left <= right {
+
+		if nums[left] <= nums[right] {
+			return nums[left]
+		}
+
+		mid := left + (right-left)>>1
+		if nums[left] <= nums[mid] {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+
+	return -1
 }
