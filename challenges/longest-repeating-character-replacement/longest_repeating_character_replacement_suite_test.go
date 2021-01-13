c9736240ac98d275498520a887914b55842297e5
aikin <1@aikin.me>
2021-01-13 16:01:17 +0800

    feat: 🎸 impl longest repeating character replacement

---
+++ challenges/longest-repeating-character-replacement/longest_repeating_character_replacement_suite_test.go
@@ -0,0 +1,13 @@
+package longest_repeating_character_replacement_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestLongestRepeatingCharacterReplacement(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "LongestRepeatingCharacterReplacement Suite")
+}
