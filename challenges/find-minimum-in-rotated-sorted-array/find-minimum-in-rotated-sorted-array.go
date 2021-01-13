59bb486a5b8ac1e488ca8963a573e51494307ecd
aikin <1@aikin.me>
2021-01-13 11:49:34 +0800

    feat: 🎸 init  find minimum in rotated sorted array

---
+++ challenges/find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -0,0 +1,5 @@
+package find_minimum_in_rotated_sorted_array
+
+func FindMin(nums []int) int {
+	return 1
+}
---
+++ challenges/find-minimum-in-rotated-sorted-array/find_minimum_in_rotated_sorted_array_suite_test.go
@@ -0,0 +1,13 @@
+package find_minimum_in_rotated_sorted_array_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestFindMinimumInRotatedSortedArray(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "FindMinimumInRotatedSortedArray Suite")
+}
