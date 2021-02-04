c0367cba91a9065a0b01212b9e3e02c16eba19e4
aikin <1@aikin.me>
2021-02-04 10:55:01 +0800

    feat: 🎸 sort negative nums by bubble sort

--- algorithms/sorting/bubble-sort/bubble_sort_test.go
+++ algorithms/sorting/bubble-sort/bubble_sort_test.go
@@ -17,4 +17,14 @@ var _ = Describe("BubbleSort", func() {
 			Ω(nums).Should(Equal([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}))
 		})
 	})
+
+	Context("When sort nums: [-1, 0, 5, -10, 20, 13, -7, 3, 2, -3]", func() {
+		It("should sorted nums: [-10, -7, -3, -1, 0, 2, 3, 5, 13, 20]", func() {
+			nums := []int{-1, 0, 5, -10, 20, 13, -7, 3, 2, -3}
+
+			Sort(nums)
+
+			Ω(nums).Should(Equal([]int{-10, -7, -3, -1, 0, 2, 3, 5, 13, 20}))
+		})
+	})
 })
